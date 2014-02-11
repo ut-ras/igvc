057803854995f9795f2e491b94ba5eff63a8e6e9
Jimmy Brisson <theotherjimmy@gmail.com>
2014-02-10 21:50:20 -0600

    made a filter function just in case we need to filter
    drunk coding for ~1 hour

--- ros_groovy_workspace/src/imu_processor/src/imu-to-heading.lisp
+++ ros_groovy_workspace/src/imu_processor/src/imu-to-heading.lisp
@@ -3,6 +3,24 @@
   (:export :converter))
 (in-package :imu-to-heading)
 
+(defun dot-product (lhs rhs)
+  (apply #'+ (mapcar #'* lhs rhs)))
+
+(defun nconvolve (func time-shifted)
+  (dot-product func (nreverse time-shifted)))
+(defun convolve (func time-shifted)
+  (dot-product func (reverse time-shifted)))
+
+(defun mk-filter (freq-response)
+  (let ((delayed-data nil)
+	(reversed-response (reverse freq-response)))
+    (lambda (sample)
+      (push sample delayed-data)
+      (if (< (length delayed-data) (length freq-response))
+	  nil
+	  (prog1 (dot-product delayed-data reversed-response)
+	    (setf delayed-data (nbutlast delayed-data)))))))
+
 (defun converter ()
   (with-ros-node ("heading")
     (let ((pub (advertise "heading" 'std_msgs-msg:Float64)))
