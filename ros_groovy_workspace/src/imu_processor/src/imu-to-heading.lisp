221217f64d1195aa30b1efaa14dbecf2a4e5969d
Jimmy Brisson <theotherjimmy@gmail.com>
2014-02-10 23:00:11 -0600

    the cl-node is growing up! it now thinks (read: converts from imu to heanding)

--- ros_groovy_workspace/src/imu_processor/src/imu-to-heading.lisp
+++ ros_groovy_workspace/src/imu_processor/src/imu-to-heading.lisp
@@ -3,9 +3,6 @@
   (:export :converter))
 (in-package :imu-to-heading)
 
-(defun dot-product (lhs rhs)
-  (apply #'+ (mapcar #'* lhs rhs)))
-
 (defun mk-filter (freq-response)
   (let ((delayed-data nil)
 	(reversed-response (reverse freq-response)))
@@ -13,13 +10,23 @@
       (push sample delayed-data)
       (if (< (length delayed-data) (length freq-response))
 	  nil
-	  (prog1 (dot-product delayed-data reversed-response)
+	  (prog1 (apply #'+ (mapcar #'* delayed-data reversed-response))
 	    (setf delayed-data (nbutlast delayed-data)))))))
 
+(defun yaw-from-quaternion (x y z rot)
+   (atan (- (* 2 y rot) (* 2 x z)) (- 1 (* 2 y y) (* 2 z z))))
+
 (defun converter ()
-  (with-ros-node ("heading")
+  (with-ros-node ("heading" :spin t)
     (let ((pub (advertise "heading" 'std_msgs-msg:Float64))
 	  (filter (mk-filter '(1/2 1/4 1/2))))
-      (loop-at-most-every .1
-	 (publish pub (make-instance 'std_msgs-msg:Float64 :data (funcall filter 1.0)))))))
-				     
+      (subscribe "/raw" 'sensor_msgs-msg:Imu
+		 (lambda (message)
+		   (let ((quat (sensor_msgs-msg:orientation-val message)))
+		     (publish pub
+		      (make-instance
+		       'std_msgs-msg:Float64
+		       :data (funcall filter
+			      (yaw-from-quaternion
+			       (geometry_msgs-msg:x-val quat) (geometry_msgs-msg:y-val quat)
+			       (geometry_msgs-msg:z-val quat) (geometry_msgs-msg:w-val quat)))))))))))
