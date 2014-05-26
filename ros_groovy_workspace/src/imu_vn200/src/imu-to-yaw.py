f96acee6662f116e8ea2e680bde712811f7dde2d
Jimmy Brisson <theotherjimmy@gmail.com>
2014-05-25 19:05:27 -0500

    fixed my math added testing to imu-to-yaw

--- ros_groovy_workspace/src/imu_vn200/src/imu-to-yaw.py
+++ ros_groovy_workspace/src/imu_vn200/src/imu-to-yaw.py
@@ -5,15 +5,29 @@ import math
 from imu_vn200.msg import vn_200_accel_gyro_compass
 from std_msgs.msg import Float64
 
+test = False
+
 if __name__ == "__main__" :
     rospy.init_node("imu_to_yaw")
     rospy.loginfo("imu to yaw node started")
-    rate = rospy.Rate(40)
+    rate = rospy.Rate(10)
     pub = rospy.Publisher( 'yaw', Float64)
     def callback ( data, pub=pub) :
-        try : pub.publish( ((math.atan(data.compass.x/data.compass.y) + (math.pi / 2)) % (2*math.pi)) - math.pi )
+        try :
+            angle = ((math.atan(data.compass.x/data.compass.y) + (math.pi / 2)) % (2*math.pi))
+            pub.publish( angle if data.compass.y > 0 else -(math.pi -angle))
         except ZeroDivisionError : pub.publish (math.py/2)
+    if test :
+        testpub = rospy.Publisher( 'raw', vn_200_accel_gyro_compass)
+        testangle = 0
     rospy.Subscriber( 'raw', vn_200_accel_gyro_compass, callback)
+        
 
     while not rospy.is_shutdown() :
+        if test :
+            newmsg = vn_200_accel_gyro_compass()
+            newmsg.compass.x = math.sin(testangle)
+            newmsg.compass.y = math.cos(testangle)
+            testangle += .1
+            testpub.publish(newmsg)
         rate.sleep()
