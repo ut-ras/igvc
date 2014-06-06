6575ed53b03a1e9413511960e5222b6624e9dd23
HAL <ut.ieee.ras@gmail.com>
2014-06-05 19:56:46 -0500

    Removed JSON parsing from embedded main.c file

--- embedded/lm4f/lm4f_node/src/main.c
+++ embedded/lm4f/lm4f_node/src/main.c
@@ -7,7 +7,6 @@
 #include <RASLib/inc/time.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/motor.h>
-#include <RASLib/inc/json_protocol.h>
 #include <RASLib/inc/servo.h>
 
 #include <StellarisWare/inc/hw_memmap.h>
@@ -22,34 +21,9 @@
 #include "handlers.h"
 #include "atospf.h"
 
-/**
-*	Publisher array for IGVC 2014 LM4F uController.
-*		Used in Main for initializations.
-*
-*	subArray:	SPLM - Set Power Left Motor=>int POWER
-*				SPRM - Set Power Right Motor=>int POWER
-*				SVLX - Set velocity of x axis(2x desired encoder ticks per message=>int VELOCITY
-*		  		SVAZ - Set velocity of about Z (2x desired encoder ticks per message)=>int ANGVELOCITY
-*			 	RSTE - Reset Encoder Values=>NONE
-*
-*	pubArray:	Publishes state information of uController to debug topic.
-*/
-tSub SPLM,SPRM,SVLX,SVAX,RSTE;
-tPub SPLMdebug, SPRMdebug, SVLXdebug, SVAXdebug;
-tSub* subArray[NUMSUB] = {&SPLM, &SPRM, &SVLX, &SVAX, &RSTE};
-tPub* pubArray[NUMPUB] = {&SPLMdebug, &SPRMdebug, &SVLXdebug, &SVAXdebug};
-
-/**
-*	Array of jsonkey strings for initializations
-*/
-char* subKey[NUMSUB] = {"SPLM", "SPRM", "SVLX", "SVAX", "RSTE"};
-char* pubKey[NUMPUB] = {"SPLMdebug", "SPRMdebug", "SVLXdebug", "SVAXdebug"};
-
 tMotor *left_motor, *right_motor;
 tEncoder *left_encoder, *right_encoder;
 tBoolean LED=true;
-
-extern float leftPower, rightPower;
 
 void blinkLED(void) {
     SetPin(PIN_F2, LED);
@@ -206,21 +180,4 @@ int main(void) {
         }
     }
     }
-    
-    /*
-	// Initialize subscribers
-	// subHandlers Array located in handlers.c
-    for(i=0;i<NUMSUB;i++) {
-	    InitializeSubscriber(subArray[i], subKey[i], 0, subHandlers[i]);
-    }
-  
-	// Initialize publishers
-    for(i=0;i<NUMPUB;i++) {
-    	InitializePublisher(pubArray[i], pubKey[i], 0, pubHandlers[i]);
-    }
-
-    BeginPublishing(.1);
-    BeginSubscribing(.1); //while(1) contained within BeginSubscribing
-    while(1);
-    */
 }
