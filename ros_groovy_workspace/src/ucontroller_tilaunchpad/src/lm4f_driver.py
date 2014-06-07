4e139826c436a2b90d946a935c0c41cdd17bb599
robz <geomasta21@aol.com>
2014-06-06 20:58:40 -0500

    now publishing on custom debug msg

--- ros_groovy_workspace/src/ucontroller_tilaunchpad/src/lm4f_driver.py
+++ ros_groovy_workspace/src/ucontroller_tilaunchpad/src/lm4f_driver.py
@@ -25,9 +25,10 @@ LISTENING_TO_NAVSTACK = False
 ### Constants
 DEBUG_TOPIC = 'lm4f_debug'
 VEL_TOPIC = 'speedometer/lm4f/vel_data'
-SUBSCRIBER = 'vel_cmd'
+SUBSCRIBER = 'cmd_vel'
 SERIAL_LINE = '/dev/lm4f'
 BAUD_RATE = 115200
+
 ### LM4F parameters to reflash board on node startup 
 rstLm4fcmd = 'make' 
 rstLm4fFlag = '-C'
@@ -80,13 +81,20 @@ def processResponse(response, debugPub, velPub):
     # Parse data into json
     try:
         respData = json.loads(response);
+        rospy.loginfo(rospy.get_name()+": LM4F response: "+str(respData))
     except:
-        rospy.loginfo(rospy.get_name()+": unable to parse LM4F response: [["+response+"]]") 
+        rospy.logerr(rospy.get_name()+": unable to parse LM4F response: [["+response+"]]") 
         return False
     
-    # Publish raw reponse 
-    rospy.loginfo(rospy.get_name()+": LM4F response: "+str(respData))
-    debugPub.publish(response) 
+    # Publish "raw" (correctly formatted) reponse 
+    debugMsg = lm4f_debug()
+    debugMsg.received_right = respData["received"]["right"]
+    debugMsg.received_left = respData["received"]["left"]
+    debugMsg.motors_right = respData["motors"]["right"]
+    debugMsg.motors_left = respData["motors"]["left"]
+    debugMsg.deltas_right = respData["deltas"]["right"]
+    debugMsg.deltas_left = respData["deltas"]["left"]
+    debugPub.publish(debugMsg)
     
     # Calculate velocity
     vel = Twist()
@@ -129,7 +137,7 @@ def sendCommand(data):
 
 def lm4fNode():
     rospy.init_node('lm4f_node', anonymous=False)
-    debugPub = rospy.Publisher(DEBUG_TOPIC, String)
+    debugPub = rospy.Publisher(DEBUG_TOPIC, lm4f_debug)
     velPub = rospy.Publisher(VEL_TOPIC, Twist)
     sub = rospy.Subscriber(
         SUBSCRIBER, 
@@ -153,17 +161,20 @@ def lm4fNode():
 
 if __name__ == '__main__':
     try:
+      
         comm = serial.Serial(
             port = SERIAL_LINE,
             baudrate = BAUD_RATE,
             timeout = .2,
             writeTimeout = 1
         )
+        """
         print 'comm initialized...'
         print 'Flashing board...'
         process = subprocess.call([rstLm4fcmd,rstLm4fFlag,rstLm4fdst,rstLm4farg])
         print 'board reset, output: '
         print process
+        """
         lm4fNode()
     except rospy.ROSInterruptException: pass
     except serial.SerialException:
